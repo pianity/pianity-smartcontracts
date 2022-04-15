68f9a39de970f5f47d09d59d3f8794b2c19e46b7
Eyal Chojnowski <eyal.choj@gmail.com>
2022-04-15 10:10:13 +0200

    :package: update declarations

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,11 +1,12 @@
 export { UNIT, PST } from "./consts";
 export * from "./contractTypes";
 export { Input, InputCodec } from "./handlers";
-export { BalanceInputCodec, BalanceInput, TickerInputCodec, TickerInput, OwnerInputCodec, OwnerInput, RoyaltiesInputCodec, RoyaltiesInput, } from "./handlers/readonlys";
+export { BalanceInputCodec, BalanceInput, VaultBalanceInputCodec, VaultBalanceInput, TotalBalanceInputCodec, TotalBalanceInput, TickerInputCodec, TickerInput, OwnerInputCodec, OwnerInput, RoyaltiesInputCodec, RoyaltiesInput, } from "./handlers/readonlys";
 export { IsApprovedForAllInputCodec, IsApprovedForAllInput, SetApprovalForAllInputCodec, SetApprovalForAllInput, } from "./handlers/approval";
 export { TransferRoyaltiesInputCodec, TransferRoyaltiesInput, TransferBatchInputCodec, TransferBatchInput, TransferInput, TransferInputCodec, } from "./handlers/transfer";
 export { MintInput, MintInputCodec } from "./handlers/mint";
 export { SettingsInput, SettingsInputCodec } from "./handlers/settings";
 export { TransactionBatchInputCodec } from "./handlers";
 export { TransactionBatchInput } from "./handlers/transactionBatch";
+export { LockInputCodec, LockInput, UnlockInputCodec, UnlockInput, IncreaseVaultInputCodec, IncreaseVaultInput, TransferLockedInputCodec, TransferLockedInput, } from "./handlers/vault";
 export { ForeignInvokeInput, ForeignInvokeInputCodec } from "./handlers/foreignInvoke";
